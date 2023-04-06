76d75b8bf793348195fbd67016fab03797a3c1da
kadenzipfel <kaden.zipfel@hotmail.com>
2023-04-06 14:43:50 -0700

    Parallelize execution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 // Import the necessary modules
+use rayon::prelude::*;
 use std::env;
+use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
 use std::time::Instant;
 use tiny_keccak::{Hasher, Keccak};
+
+const MAX_NONCE: u32 = std::u32::MAX;
 
 // Function to calculate the Keccak-256 hash
 fn keccak256(input: &[u8]) -> [u8; 32] {
@@ -17,22 +21,22 @@ fn mine_function_selector(
     name: &str,
     params: &str,
     leading_zeros: usize,
-) -> Option<(String, String)> {
-    let mut nonce = 0;
-    loop {
+    nonce_start: u32,
+    nonce_step: u32,
+    found: &AtomicBool,
+) -> Option<(u32, String, String)> {
+    let mut nonce = nonce_start;
+    while nonce < MAX_NONCE && !found.load(Ordering::Relaxed) {
         let input = format!("{}{}{}", name, nonce, params);
         let hash = keccak256(input.as_bytes());
         let selector = &hash[..4];
 
         if selector.iter().take(leading_zeros).all(|&byte| byte == 0) {
-            return Some((hex::encode(selector), input));
+            found.store(true, Ordering::Relaxed);
+            return Some((nonce, hex::encode(selector), input));
         }
 
-        if nonce == std::u32::MAX {
-            break;
-        }
-
-        nonce += 1;
+        nonce += nonce_step;
     }
     None
 }
@@ -54,10 +58,30 @@ fn main() {
         }
     };
 
+    let num_threads = num_cpus::get();
     let start_time = Instant::now();
+    let found = AtomicBool::new(false);
 
-    match mine_function_selector(function_name, params, leading_zeros) {
-        Some((selector, signature)) => {
+    let results: Vec<Option<(u32, String, String)>> = (0..num_threads)
+        .into_par_iter()
+        .map(|i| {
+            let nonce_start = i as u32;
+            let nonce_step = num_threads as u32;
+            mine_function_selector(
+                function_name,
+                params,
+                leading_zeros,
+                nonce_start,
+                nonce_step,
+                &found,
+            )
+        })
+        .collect();
+
+    let result = results.into_iter().filter_map(|r| r).min_by_key(|r| r.0);
+
+    match result {
+        Some((_nonce, selector, signature)) => {
             let elapsed_time = start_time.elapsed();
             println!("Function selector: {}, Signature: {}", selector, signature);
             println!(
